0dfd4f1ddbab4f2ca3c0ab6622795a83c7569a06
Alex Santos de Souza <alexssmusica@gmail.com>
2020-06-01 14:12:41 -0300

    Login Habilitado

--- clientes/src/main/java/io/github/dougllasfps/clientes/config/WebConfig.java
+++ clientes/src/main/java/io/github/dougllasfps/clientes/config/WebConfig.java
@@ -1,36 +1,36 @@
-// package io.github.dougllasfps.clientes.config;
+package io.github.dougllasfps.clientes.config;
 
-// import java.util.Arrays;
-// import java.util.List;
+import java.util.Arrays;
+import java.util.List;
 
-// import org.springframework.boot.web.servlet.FilterRegistrationBean;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.core.Ordered;
-// import org.springframework.web.cors.CorsConfiguration;
-// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-// import org.springframework.web.filter.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-// @Configuration
-// public class WebConfig {
+@Configuration
+public class WebConfig {
 
-//     @Bean
-//     public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean(){
-//         List<String> all = Arrays.asList("*");
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean(){
+        List<String> all = Arrays.asList("*");
 
-//         CorsConfiguration corsConfiguration = new CorsConfiguration();
-//         corsConfiguration.setAllowedOrigins(all);
-//         corsConfiguration.setAllowedHeaders(all);
-//         corsConfiguration.setAllowedMethods(all);
-//         corsConfiguration.setAllowCredentials(true);
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(all);
+        corsConfiguration.setAllowedHeaders(all);
+        corsConfiguration.setAllowedMethods(all);
+        corsConfiguration.setAllowCredentials(true);
 
-//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//         source.registerCorsConfiguration("/**", corsConfiguration);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
 
-//         CorsFilter corsFilter = new CorsFilter(source);
-//         FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<>(corsFilter);
-//         filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        CorsFilter corsFilter = new CorsFilter(source);
+        FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<>(corsFilter);
+        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
-//         return filter;
-//     }
-// }
+        return filter;
+    }
+}
--- clientes/src/main/java/io/github/dougllasfps/clientes/service/UsuarioService.java
+++ clientes/src/main/java/io/github/dougllasfps/clientes/service/UsuarioService.java
@@ -1,41 +1,42 @@
-// package io.github.dougllasfps.clientes.service;
+package io.github.dougllasfps.clientes.service;
 
-// import io.github.dougllasfps.clientes.exception.UsuarioCadastradoException;
-// import io.github.dougllasfps.clientes.model.entity.Usuario;
-// import io.github.dougllasfps.clientes.model.repository.UsuarioRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-// @Service
-// public class UsuarioService implements UserDetailsService {
+import io.github.dougllasfps.clientes.exception.UsuarioCadastradoException;
+import io.github.dougllasfps.clientes.model.entity.Usuario;
+import io.github.dougllasfps.clientes.model.repository.UsuarioRepository;
 
-//     @Autowired
-//     private UsuarioRepository repository;
+@Service
+public class UsuarioService implements UserDetailsService {
 
-//     public Usuario salvar(Usuario usuario){
-//         boolean exists = repository.existsByUsername(usuario.getUsername());
-//         if(exists){
-//             throw new UsuarioCadastradoException(usuario.getUsername());
-//         }
-//         return repository.save(usuario);
-//     }
+    @Autowired
+    private UsuarioRepository repository;
 
-//     @Override
-//     public UserDetails loadUserByUsername( String username ) throws UsernameNotFoundException {
-//         Usuario usuario = repository
-//                             .findByUsername(username)
-//                             .orElseThrow(() -> new UsernameNotFoundException("Login não encontrado.") );
+    public Usuario salvar(Usuario usuario){
+        boolean exists = repository.existsByUsername(usuario.getUsername());
+        if(exists){
+            throw new UsuarioCadastradoException(usuario.getUsername());
+        }
+        return repository.save(usuario);
+    }
 
-//         return User
-//                 .builder()
-//                 .username(usuario.getUsername())
-//                 .password(usuario.getPassword())
-//                 .roles("USER")
-//                 .build()
-//                 ;
-//     }
-// }
+    @Override
+    public UserDetails loadUserByUsername( String username ) throws UsernameNotFoundException {
+        Usuario usuario = repository
+                            .findByUsername(username)
+                            .orElseThrow(() -> new UsernameNotFoundException("Login não encontrado.") );
+
+        return User
+                .builder()
+                .username(usuario.getUsername())
+                .password(usuario.getPassword())
+                .roles("USER")
+                .build()
+                ;
+    }
+}
